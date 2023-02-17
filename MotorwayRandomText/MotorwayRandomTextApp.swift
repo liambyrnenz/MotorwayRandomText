dcdd687599ce159a67747405010667e9391c1919
Liam Byrne <liam.jb1@gmail.com>
2023-02-18 12:57:47 +1300

    Initial Commit

---
+++ MotorwayRandomText/MotorwayRandomTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotorwayRandomTextApp.swift
+//  MotorwayRandomText
+//
+//  Created by Liam on 18/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MotorwayRandomTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
