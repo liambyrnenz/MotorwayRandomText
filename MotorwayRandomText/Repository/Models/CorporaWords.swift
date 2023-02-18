cb036654b1fee8ed12b3a2ba27986e307abaa92f
Liam Byrne <liam.jb1@gmail.com>
2023-02-18 19:00:10 +1300

    Create repository and models for retrieving words from JSON

---
+++ MotorwayRandomText/Repository/Models/CorporaWords.swift
@@ -0,0 +1,11 @@
+//
+//  CorporaWords.swift
+//  MotorwayRandomText
+//
+//  Created by Liam on 18/02/23.
+//
+
+struct CorporaWords: Decodable {
+    let description: String?
+    let commonWords: [String]
+}
